62700532c333309a72e4bde90cc8ea084d780c78
Akorede Afolabi <brainconnect0@gmail.com>
2024-11-25 13:38:08 +0100

    Update the initialState of appSlice to include the Add and Edit task modal

--- src/redux/features/appSlice.ts
+++ src/redux/features/appSlice.ts
@@ -8,6 +8,13 @@ const initialState = {
     isOpen: false,
     variant: "",
   },
+  isAddAndEditTaskModal: {
+    isOpen: false,
+    variant: "",
+    title: "",
+    index: -1,
+    name: "" 
+  },
 };
 
 export const features = createSlice({
@@ -30,6 +37,22 @@ export const features = createSlice({
       state.isAddAndEditBoardModal.isOpen = false;
       state.isAddAndEditBoardModal.variant = "";
     },
+
+    openAddAndEditTaskModal: (state, { payload }) => {
+      state.isAddAndEditTaskModal.isOpen = true;
+      state.isAddAndEditTaskModal.variant = payload.variant;
+      state.isAddAndEditTaskModal.title = payload.title;
+      state.isAddAndEditTaskModal.index = payload.index;
+      state.isAddAndEditTaskModal.name = payload.name;
+    },
+
+    closeAddAndEditTaskModal: (state) => {
+      state.isAddAndEditTaskModal.isOpen = false;
+      state.isAddAndEditTaskModal.variant = "";
+      state.isAddAndEditTaskModal.title = "";
+      state.isAddAndEditTaskModal.index = -1;
+      state.isAddAndEditTaskModal.name = "";
+    },
   },
 });
 
@@ -37,13 +60,20 @@ export const features = createSlice({
 export const { 
   setPageTitle,
   openAddAndEditBoardModal,
-  closeAddAndEditBoardModal
+  closeAddAndEditBoardModal,
+  openAddAndEditTaskModal,
+  closeAddAndEditTaskModal
 } = features.actions;
 
 // Selector function to retrieve the current board name from the state
 export const getPageTitle = (state: RootState) => state.features.currentBoardName;
 export const getAddAndEditBoardModalValue = (state: RootState) => state.features.isAddAndEditBoardModal.isOpen;
 export const getAddAndEditBoardModalVariantValue = (state: RootState) => state.features.isAddAndEditBoardModal.variant;
+export const getAddAndEditTaskModalValue = (state: RootState) => state.features.isAddAndEditTaskModal.isOpen;
+export const getAddAndEditTaskModalVariantValue = (state: RootState) => state.features.isAddAndEditTaskModal.variant;
+export const getAddAndEditTaskModalTitleValue = (state: RootState) => state.features.isAddAndEditTaskModal.title;
+export const getAddAndEditTaskModalIndexValue = (state: RootState) => state.features.isAddAndEditTaskModal.index;
+export const getAddAndEditTaskModalNameValue = (state: RootState) => state.features.isAddAndEditTaskModal.name;
 
 // Export the reducer for use in the Redux store
 export default features.reducer;
